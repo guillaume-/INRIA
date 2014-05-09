225bebffd9162dbc0fdd3a1f91b7a8f8f87e3b49
guillaume- <guillaume.ri.du@gmail.com>
2014-05-09 09:21:59 +0200

    Update main.ml

--- main.ml
+++ main.ml
@@ -4,14 +4,12 @@ let main () =
       Ms_parser.g_specification Ms_scanner.lexer lexbuf
   with End_of_file -> exit 0
 
-let t0 = Ter_parser_checks.chk_spec (main());;
+let t0 = Ter_iterateurs.check (main());;
 
-let t1 = Ter_convert.cvrt_spec t0;;
+let t1 = Ter_iterateurs.addCall t0;;
 
-(*
-let t2 = Ter_iterateurs.do_transfo t1;;
-*)
+let t2 = Ter_iterateurs.noSub t1;;
 
-let t3 = Ter_toString.str_specification t1;;
+let t3 = Ter_toString.str_specification t2;;
 
 print_string t3
