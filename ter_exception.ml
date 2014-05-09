ee524c8b6ff946dd39b4ef84c66dff6fc20922d3
guillaume- <guillaume.ri.du@gmail.com>
2014-05-09 09:21:33 +0200

    Update ter_exception.ml

--- ter_exception.ml
+++ ter_exception.ml
@@ -1,2 +1,9 @@
 exception Bad_construction of string;;
 exception Incompatible_outputs;;
+exception Multiple_definition of string;;
+exception Undefined of string;;
+exception Incompatible_definitions of string;;
+exception Type_mismatch of string;;
+exception Invalide_argument_numbers of string;;
+exception Incompatible_direction of string;;
+exception Incorrect_call of string;;
