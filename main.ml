dc88f40b24c76f297063aff31545c87423de8610
AgatheD <agathe.duyck@gmail.com>
2014-05-12 10:09:46 +0200

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,15 +1,25 @@
+open Ter_transfos
+
 let main () =
   try
     let lexbuf = Lexing.from_channel stdin in
       Ms_parser.g_specification Ms_scanner.lexer lexbuf
   with End_of_file -> exit 0
 
-let t0 = Ter_iterateurs.check (main());;
+let spec_0 = print_string "----------------------- Lecture ficher -----------------------\n";
+			let s = main() 
+			in print_string "------------------ Verification syntaxique -------------------\n";
+				check (s);;
 
-let t1 = Ter_iterateurs.addCall t0;;
 
-let t2 = Ter_iterateurs.noSub t1;;
 
-let t3 = Ter_toString.str_specification t2;;
-
-print_string t3
+(*let spec_1 = print_string "-- Remplacement symboles arithmétiques -> appels procedures --\n";
+			addCall spec_0;;*)
+			
+let spec_2 = print_string "----------------- Suppression des submodules -----------------\n";
+			noSub spec_0;;
+			
+(*let spec_3 = check spec_2;;*)
+			
+let str = Ter_toString.str_specification spec_2;;
+print_string str;
