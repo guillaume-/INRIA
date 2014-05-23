9f55c20f4a3ef5ac65874be9642adc85a53f870e
AgatheD <agathe.duyck@gmail.com>
2014-05-23 10:01:17 +0200

    Update ter_transfos.ml

--- ter_transfos.ml
+++ ter_transfos.ml
@@ -3,7 +3,8 @@ open Ter_identite
 open Ter_arith_to_call
 open Ter_no_submod
 open Ter_chk_spec
-open Ter_schemas_latex
+(*open Ter_schemas_latex*)
+open Ter_schemas_dot
 
 let noSub prog =
 	let module Trans = Tfr_no_submodule
@@ -13,14 +14,28 @@ let noSub prog =
 let addCall prog =
 	let module Trans = Tfr_arith_to_call
 	in let module Apply_transfo = Transformation(Trans) 
-	in Apply_transfo.transform_spec prog
+		in Apply_transfo.transform_spec prog
 
 let check prog =
-	let module Trans = Tfr_chk_spec
+	let module Applic = Apl_chk_spec
+	in let module Apply_transfo = Application(Applic) 
+		in Apply_transfo.apply_spec prog 
+
+let id prog = 
+	let module IdParam : tRef = struct
+		type r = unit
+		let creerRef _ = ()
+	end 
+	in let module Trans = Identite(IdParam)
 	in let module Apply_transfo = Transformation(Trans) 
 		in Apply_transfo.transform_spec prog 
 
-let schemas prog =
+(*let schemas_latex prog =
 	let module Trans = Ter_schemas_latex
+	in let module Apply_transfo = Transformation(Trans) 
+		in let p = Apply_transfo.get_param prog in p.res*)
+		
+let schemas_dot prog =
+	let module Trans = Ter_schemas_dot
 	in let module Apply_transfo = Transformation(Trans) 
 		in let p = Apply_transfo.get_param prog in p.res
