58d6d06ad62c01476a1187bbbdeba2ccbb5f3ed7
guillaume- <guillaume.ri.du@gmail.com>
2014-06-24 09:36:12 +0200

    Update ter_transfos.ml

--- ter_transfos.ml
+++ ter_transfos.ml
@@ -3,9 +3,9 @@ open Ter_identite
 open Ter_arith_to_call
 open Ter_no_submod
 open Ter_chk_spec
-(*open Ter_schemas_latex*)
-open Ter_schemas_dot
-open Ter_to_string
+(*open Ter_schemas_dot*)
+(*open Ter_schemas_latex *)
+open Ter_make_box
 
 let id prog = 
 	let module IdParam : tRef = struct
@@ -24,24 +24,28 @@ let noSub prog =
 let addCall prog =
 	let module Trans = Tfr_arith_to_call
 	in let module Apply_transfo = Transformation(Trans) 
-		in Apply_transfo.transform_spec prog
+	in Apply_transfo.transform_spec prog
 
 let check prog =
 	let module Applic = Apl_chk_spec
 	in let module Apply_transfo = Application(Applic) 
 		in Apply_transfo.apply_spec prog 
 
-(*let schemas_latex prog =
+(*let schemas prog =
 	let module Trans = Ter_schemas_latex
 	in let module Apply_transfo = Transformation(Trans) 
-		in let p = Apply_transfo.get_param prog in p.res*)
-		
-let schemas_dot prog =
+		in let p = Apply_transfo.get_param prog in p.res
+*)
+
+(*let schemas_dot prog =
 	let module Trans = Ter_schemas_dot
 	in let module Apply_transfo = Transformation(Trans) 
 		in let p = Apply_transfo.get_param prog in p.res
-		
-let to_string prog = 
-	let module Applic = Apl_to_string
-	in let module Apply_transfo = Application(Applic)
-		in let p = (Apply_transfo.apply_spec prog) in p.str
+*)
+
+let mk_box prog =
+	let module Applic = Ter_make_box
+	in let module Apply_transfo = Application(Applic) 
+		in let p = Apply_transfo.apply_spec prog
+		   in p.res
+	
